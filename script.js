5cbd4abee3673ce9a909d7c3bf1b1086ea0b0e99
tasosbeast <66928134+tasosbeast@users.noreply.github.com>
2023-11-26 04:31:50 +0200

    Refactor grid creation and add clear and resize functionality

--- script.js
+++ script.js
@@ -5,25 +5,65 @@ function getRandomInt(max) {
 }
 
 function createDivs(rows, cols) {
-  for (let i = 0; i < rows * cols; i++) {
-    const divEl = document.createElement("div");
-    divEl.classList.add("grid-item");
-    divEl.style.backgroundColor = "rgb(255, 255, 255)";
-    divEl.addEventListener("mouseover", function () {
-      divEl.style.backgroundColor = `rgb(${getRandomInt(255)}, ${getRandomInt(
-        255
-      )}, ${getRandomInt(255)})`;
+  for (let i = 0; i < rows; i++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.style.display = "flex";
+    rowDiv.style.flexDirection = "row";
+    rowDiv.style.justifyContent = "center";
+    rowDiv.style.alignItems = "center";
+    rowDiv.style.width = "100%";
+    gridDivEl.appendChild(rowDiv);
 
-      divEl.style.transition = "background-color 0.1s ease";
-    });
+    for (let j = 0; j < cols; j++) {
+      const colDiv = document.createElement("div");
+      colDiv.classList.add("grid-item");
+      // Add your event listeners and other styles here
+      colDiv.addEventListener("mouseover", () => {
+        colDiv.style.backgroundColor = `rgb(${getRandomInt(
+          255
+        )}, ${getRandomInt(255)}, ${getRandomInt(255)})`;
+        colDiv.style.transition = "background-color 0.1s ease";
+      });
 
-    divEl.addEventListener("mouseleave", function () {
-      divEl.style.backgroundColor = "rgb(255, 255, 255)";
-      divEl.style.transition = "background-color 5s ease";
-    });
+      colDiv.addEventListener("mouseout", () => {
+        colDiv.style.backgroundColor = "black";
+        colDiv.style.transition = "background-color 5s ease";
+      });
 
-    gridDivEl.appendChild(divEl);
+      rowDiv.appendChild(colDiv);
+
+      // colDiv.style.backgroundColor = "black";
+    }
   }
 }
 
-createDivs(10, 10);
+function clearGrid() {
+  const gridItems = document.querySelectorAll(".grid-item");
+  gridItems.forEach((item) => {
+    item.style.backgroundColor = "black";
+    item.style.transition = "background-color 1s ease";
+  });
+}
+
+function changeGridSize() {
+  const newSize = prompt("Enter new size");
+  const gridItems = document.querySelectorAll(".grid-item");
+  gridItems.forEach((item) => {
+    item.remove();
+  });
+  if (newSize > 100) {
+    alert("Please enter a number less than 100");
+  } else if (newSize < 1) {
+    alert("Please enter a number greater than 0");
+  } else {
+    createDivs(newSize, newSize);
+  }
+}
+
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", clearGrid);
+
+const changeSizeBtn = document.querySelector(".resize");
+changeSizeBtn.addEventListener("click", changeGridSize);
+
+createDivs(15, 15);
